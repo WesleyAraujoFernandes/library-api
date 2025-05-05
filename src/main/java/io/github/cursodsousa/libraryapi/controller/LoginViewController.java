7bd6016626d0cb8d3c9fd0a4009593d89e2f89c0
WesleyAraujoFernandes <wesley@viaapia.com.br>
2025-05-04 22:44:45 -0300

    Commit inicial.

--- src/main/java/io/github/cursodsousa/libraryapi/controller/LoginViewController.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/LoginViewController.java
@@ -20,6 +20,6 @@ public class LoginViewController {
         if (authentication instanceof CustomAuthentication customAuth) {
             System.out.println(customAuth.getUsuario());
         }
-        return "Olá " + authentication.getName();
+        return "Olá, " + authentication.getName();
     }
 }
