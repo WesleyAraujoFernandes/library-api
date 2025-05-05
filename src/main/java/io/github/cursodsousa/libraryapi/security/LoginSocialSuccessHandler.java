ca83faed8e1971ee588efc5f7f858f5bf0929652
WesleyAraujoFernandes <wesley@viaapia.com.br>
2025-05-05 15:18:52 -0300

    Criadas as configurações de segurança no arquivo .env

--- src/main/java/io/github/cursodsousa/libraryapi/security/LoginSocialSuccessHandler.java
+++ src/main/java/io/github/cursodsousa/libraryapi/security/LoginSocialSuccessHandler.java
@@ -1,9 +1,11 @@
 package io.github.cursodsousa.libraryapi.security;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -19,10 +21,13 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+    private static final String SENHA_PADRAO = "321";
+
     private final UsuarioService usuarioService;
+
     @Override
     public void onAuthenticationSuccess(
-            HttpServletRequest request, 
+            HttpServletRequest request,
             HttpServletResponse response,
             Authentication authentication) throws ServletException, IOException {
         super.onAuthenticationSuccess(request, response, authentication);
@@ -33,10 +38,31 @@ public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSu
 
         Usuario usuario = usuarioService.obterPorEmail(email);
 
+        if (usuario == null) {
+            usuario = cadastrarUsuarioNaBase(email);
+        }
+
         authentication = new CustomAuthentication(usuario);
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         super.onAuthenticationSuccess(request, response, authentication);
+    }
+
+    private Usuario cadastrarUsuarioNaBase(String email) {
+        Usuario usuario;
+        usuario = new Usuario();
+        usuario.setEmail(email);
+
+        usuario.setLogin(obterLoginApartirEmail(email));
+
+        usuario.setSenha(SENHA_PADRAO);
+        usuario.setRoles(List.of("OPERADOR"));
+        usuarioService.salvar(usuario);
+        return usuario;
+    }
+
+    private String obterLoginApartirEmail(String email) {
+        return email.substring(0, email.indexOf("@"));
     }
 }
